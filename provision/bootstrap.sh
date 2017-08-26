ecf92c0b7a123aa44e1823326116b50f3b2cdbcc
Adrian Solumsmo <adrian.solumsmo@gmail.com>
2017-08-26 18:29:08 +0200

    adds extra php packages and mariadb-server

--- provision/bootstrap.sh
+++ provision/bootstrap.sh
@@ -4,8 +4,17 @@
 apt-get update
 apt-get install --yes silversearcher-ag
 apt-get install --yes nginx
-apt-get install --yes mysql-server
-apt-get install --yes php-fpm php-mysql
+apt-get install --yes mariadb-server
+apt-get install --yes php-fpm
+
+# PHP libraries
+apt-get install --yes php-mysql
+apt-get install --yes php-curl
+apt-get install --yes php-gd
+apt-get install --yes php-mbstring
+apt-get install --yes php-mcrypt
+apt-get install --yes php-xml
+apt-get install --yes php-xmlrpc
 
 # configure nginx
 sudo cp /vagrant/provision/nginx.conf /etc/nginx/sites-available/site.conf
@@ -13,12 +22,12 @@ sudo chmod 644 /etc/nginx/sites-available/site.conf
 sudo ln -sf /etc/nginx/sites-available/site.conf /etc/nginx/sites-enabled/site.conf
 sudo service nginx restart
 
-# configure mysql
-sudo mysql --user=root --password=root --execute="\
+# configure mariadb
+sudo mariadb --user=root --password=root --execute="\
 DROP USER IF EXISTS 'root'@'localhost';\
 CREATE USER IF NOT EXISTS 'root'@'%' identified by 'root';\
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';\
-FLUSH PRIVILEGES;"
+FLUSH PRIVILEGES;" > /dev/null 2>&1
 
 # configure php
 systemctl restart php7.0-fpm 
